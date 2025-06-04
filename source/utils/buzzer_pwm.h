0c07ecbf06d482f6aa49f87f6bdb28c446b3d9b8
Sufian Bin Hossain Adib <140370019+adibhossain3698@users.noreply.github.com>
2025-06-04 19:21:33 +0200

    memory game done

---
+++ source/utils/buzzer_pwm.h
@@ -0,0 +1,8 @@
+#ifndef BUZZER_PWM_H
+#define BUZZER_PWM_H
+
+void buzzer_pwm_init(void);
+void buzzer_beep_error(void);
+void buzzer_beep_win(void);
+
+#endif
---
+++ source/utils/led_control.h
@@ -0,0 +1,10 @@
+#ifndef LED_CONTROL_H
+#define LED_CONTROL_H
+
+#include <stdint.h>
+
+void leds_init(void);
+void led_on(uint8_t led_number);
+void led_off(uint8_t led_number);
+
+#endif
