3ec2125fa165dd98070f3f13437f474b5f387218
Sufian Bin Hossain Adib <adibhossainst123@gmail.com>
2025-06-11 00:53:10 +0200

    ctimer1-pwm uploded

--- source/ctimer1_pwm.c
+++ source/ctimer1_pwm.c
@@ -1,31 +1,84 @@
-/*
- * ctimer1_pwm.c
+/*! ***************************************************************************
  *
- *  Created on: 7 Jun 2025
- *      Author: adibh
- */
+ * \brief     Main application
+ * \file      main.c
+ * \author    Hugo Arends
+ * \date      February 2024
+ *
+ * \see       NXP. (2024). MCX A153, A152, A143, A142 Reference Manual. Rev. 4,
+ *            01/2024. From:
+ *            https://www.nxp.com/docs/en/reference-manual/MCXAP64M96FS3RM.pdf
+ *
+ * \copyright 2024 HAN University of Applied Sciences. All Rights Reserved.
+ *            \n\n
+ *            Permission is hereby granted, free of charge, to any person
+ *            obtaining a copy of this software and associated documentation
+ *            files (the "Software"), to deal in the Software without
+ *            restriction, including without limitation the rights to use,
+ *            copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *            copies of the Software, and to permit persons to whom the
+ *            Software is furnished to do so, subject to the following
+ *            conditions:
+ *            \n\n
+ *            The above copyright notice and this permission notice shall be
+ *            included in all copies or substantial portions of the Software.
+ *            \n\n
+ *            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *            EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *            OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *            NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *            HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *            WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *            FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *            OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *****************************************************************************/
+
+#include <MCXA153.h>
 #include "ctimer1_pwm.h"
-#include "fsl_clock.h"
-#include "fsl_iocon.h"
-#include "fsl_ctimer.h"
 
-#define CTIMER            CTIMER1
-#define CTIMER_MAT_OUT    kCTIMER_Match_0
-#define PWM_FREQ_HZ       5000
+// -----------------------------------------------------------------------------
+// Local type definitions
+// -----------------------------------------------------------------------------
 
-void PWM_Init(void) {
-    CLOCK_EnableClock(kCLOCK_Ctimer1);
-    IOCON_PinMuxSet(IOCON, 3, 14, IOCON_FUNC4); // P3_14 as CTIMER1_MAT0
+// -----------------------------------------------------------------------------
+// Local function prototypes
+// -----------------------------------------------------------------------------
 
-    ctimer_config_t config;
-    CTIMER_GetDefaultConfig(&config);
-    CTIMER_Init(CTIMER, &config);
+// -----------------------------------------------------------------------------
+// Local variables
+// -----------------------------------------------------------------------------
 
-    CTIMER_SetupPwm(CTIMER, CTIMER_MAT_OUT, PWM_FREQ_HZ, 0.5f, false);
-    CTIMER_StartTimer(CTIMER);
+// -----------------------------------------------------------------------------
+// Main application
+// -----------------------------------------------------------------------------
+int main(void)
+{
+    ctimer1_pwm_init();
+
+    while (1)
+    {
+        // Loop to change duty cycle to simulate intensity control
+        for (int duty = 50; duty <= 450; duty += 50)
+        {
+            PWM0->SM[2].VAL[2] = -duty;
+            PWM0->SM[2].VAL[3] = duty;
+            PWM0->MCTRL |= PWM_MCTRL_LDOK(1 << 2);
+
+            for (volatile int i = 0; i < 500000; i++);
+        }
+
+        for (int duty = 400; duty >= 50; duty -= 50)
+        {
+            PWM0->SM[2].VAL[2] = -duty;
+            PWM0->SM[2].VAL[3] = duty;
+            PWM0->MCTRL |= PWM_MCTRL_LDOK(1 << 2);
+
+            for (volatile int i = 0; i < 500000; i++);
+        }
+    }
 }
 
-void PWM_SetDutyCycle(uint8_t dutyCyclePercent) {
-    float dutyCycle = (float)dutyCyclePercent / 100.0f;
-    CTIMER_UpdatePwmDutycycle(CTIMER, CTIMER_MAT_OUT, dutyCycle);
-}
+// -----------------------------------------------------------------------------
+// Local function implementation
+// -----------------------------------------------------------------------------
